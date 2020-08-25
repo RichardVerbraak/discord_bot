c137186b1ae057accbd849468cdd7b93294b976b
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-08-25 17:23:35 +0200

    Simple message event so the bot responds hello to 'hello'

--- src/bot.js
+++ src/bot.js
@@ -2,9 +2,19 @@ require('dotenv').config()
 
 const { Client } = require('discord.js')
 const client = new Client()
+const PREFIX = '$'
 
 client.on('ready', () => {
 	console.log(`${client.user.tag} has logged in.`)
+})
+
+client.on('message', (message) => {
+	// If bot, don't reply a message
+	if (message.author.bot) return
+	console.log(`${message.author.tag}: ${message.content}`)
+	if (message.content === 'hello') {
+		message.channel.send('hello')
+	}
 })
 
 client.login(process.env.DISCORDJS_BOT_TOKEN)
